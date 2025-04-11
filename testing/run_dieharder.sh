b2e67b59b178c570948cbc1df608a632716cf3e0
ayoub.binnakhi <ayoub.binnakhi@ucalgary.ca>
2025-04-11 13:09:31 -0600

    Fixed usage

--- testing/run_dieharder.sh
+++ testing/run_dieharder.sh
@@ -6,6 +6,6 @@ mkdir -p diehard_results
 for file in diehard_inputs/*.bin; do
     base=$(basename "$file" .bin)
     echo "[→] Testing $base..."
-    dieharder -d 0 -d 3 -d 4 -d 10 -d 11 -g 201 -f "$file" > "diehard_results/${base}_results.txt"
+    dieharder -a -g 201 -f "$file" > "diehard_results/${base}_results.txt"
     echo "[✔] Results saved to diehard_results/${base}_results.txt"
 done
